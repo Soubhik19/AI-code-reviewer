2ebdf33642782fa918dd2bd55cdb856461c67705
Soubhik Samanta <soubhiksamanta25@gmail.com>
2025-08-15 15:16:47 +0530

    add AI service and routes for content generation

---
+++ BackEnd/src/controllers/ai.controller.js
@@ -0,0 +1,12 @@
+const aiService = require('../Services/ai.services');
+
+module.exports.getResponse=async(req,res)=>{
+const prompt = req.query.prompt;
+
+if(!prompt) {
+    return res.status(400).send('Prompt is required');
+}
+const response = await aiService.generateContent(prompt);
+res.send(response);
+
+}
