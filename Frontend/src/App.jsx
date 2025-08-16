ebab1fc5aeb4412bc26405ac87c1f2b2a20de3d4
Soubhik Samanta <soubhiksamanta25@gmail.com>
2025-08-16 15:35:20 +0530

    add react-simple-code-editor for enhanced code editing experience in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState ,useEffect} from 'react'
 import "prismjs/themes/prism-tomorrow.css"
 import prism from 'prismjs'
+import Editor from 'react-simple-code-editor'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [code, setCode] = useState(`function sum() {function sum() { return 1 + 1; }
+  }`);
+  
+
 
   useEffect(()=>{
     prism.highlightAll()
@@ -15,16 +20,21 @@ function App() {
     <main>
       <div className="left">
         <div className="code">
-          <pre>
-            <code className="language-javascript">
-              {`
-                function sum(){
-                  return 1+2;
-                  `
-                }
-              
-            </code>
-          </pre>
+        <Editor
+          value={code}
+          onValueChange={code => setCode(code)}
+          highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+          padding={10}
+          style={{
+            fontFamily: '"Fira Code", "Fira Mono", monospace',
+            fontSize: "16px",
+            border: "1px solid #ddd",
+            borderRadius: "5px",
+            height: "100%",
+            width: "100%"
+  }}
+/>
+
         </div>
         <div className="review">Review</div>
       </div>
